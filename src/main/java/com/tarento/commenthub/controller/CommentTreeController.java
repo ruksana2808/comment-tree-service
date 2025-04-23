2e3e2e8a1a3bf604d9df339accd14ce5db2447c1
ruksanasemeir <ruksana.semeir@tarento.com>
2025-04-23 16:39:12 +0530

    Made changes according to review

--- src/main/java/com/tarento/commenthub/controller/CommentTreeController.java
+++ src/main/java/com/tarento/commenthub/controller/CommentTreeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("commentTree")
+@RequestMapping("commentTree/v1")
 @Slf4j
 public class CommentTreeController {
 
