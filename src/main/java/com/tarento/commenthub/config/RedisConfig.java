1aa3ad44ad87bf7b030f95bb916050a9d05ec9ba
ruksanasemeir <ruksana.semeir@tarento.com>
2025-04-24 20:24:04 +0530

    Redis config issue resolved

--- src/main/java/com/tarento/commenthub/config/RedisConfig.java
+++ src/main/java/com/tarento/commenthub/config/RedisConfig.java
@@ -2,6 +2,7 @@
 package com.tarento.commenthub.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,12 +12,27 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+  @Value("${spring.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.redis.port}")
+  private int redisPort;
+
   @Bean
   public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
     RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(connectionFactory);
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     return redisTemplate;
+  }
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory factory =
+        new org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory(redisHost,
+            redisPort);
+    factory.afterPropertiesSet();
+    return factory;
   }
 
 }
